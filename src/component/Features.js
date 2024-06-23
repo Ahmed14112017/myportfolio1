e723281430dd0cc7c54e259f487a76ef35fc5054
ahmed arafa <70607269+Ahmed14112017@users.noreply.github.com>
2024-06-23 23:03:08 +0300

    3

--- src/component/Features.js
+++ src/component/Features.js
@@ -1,9 +1,155 @@
 import React from 'react'
+import { MdMenu } from "react-icons/md";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { SiWindows } from "react-icons/si";
+import { SiProgress } from "react-icons/si";
+import { FaMobile } from "react-icons/fa";
+import { SiAntdesign } from "react-icons/si";
+import { FaGlobe } from "react-icons/fa";
+
+
+
+
+
 
 export default function Features() {
   return (
-    <div>
-      Features
-    </div>
+    <section id='features' className='w-full py-20 border-b-[1px] border-b-black '>
+      <div className='flex flex-col gap-6'>
+        <h4 className='text-designColor'>FEATURES</h4>
+        <h1 className='text-6xl font-bold text-lightText pb-5'>What I Do</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20 '>
+                <div className='flex flex-col gap-4 shadow-shadowOne h-80 w-full px-10 py-8 '>
+                  <div className='h-72 overflow-y-hidden'>
+                    <div className='feature-box h-full translate-y-14 bg-slate-100 flex flex-col gap-4'>
+                      <div  className=' w-10 h-8 flex flex-col justify-between '>
+                      <MdMenu className='text-4xl text-designColor'/>
+                      </div>
+                  <div className='flex flex-col gap-6' >
+                  <h3 className="text-xl md:text-2xl font-titleFont font-bold text-gray-300">Business Stratagy</h3>
+                  <p className='font-titleFont text-lightText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Atque soluta hic consequuntur eum repellendus ad.</p>
+                     <span className="text-2xl text-designColor">
+                     <FaLongArrowAltRight/>
+                     </span>
+                     
+                  </div>
+                 
+                    </div>
+                  
+                  </div>
+                 
+                </div>
+                <div className='flex flex-col gap-4 shadow-shadowOne h-80 w-full px-10 py-8 '>
+                  <div className='h-72 overflow-y-hidden'>
+                    <div className='feature-box h-full translate-y-14 bg-slate-100 flex flex-col gap-4'>
+                      <div  className=' w-10 h-8 flex flex-col justify-between '>
+                      <SiWindows className='text-4xl text-designColor'/>
+                      
+                      </div>
+                  <div className='flex flex-col gap-6' >
+                  <h3 className="text-xl md:text-2xl font-titleFont font-bold text-gray-300">Business Stratagy</h3>
+                  <p className='font-titleFont text-lightText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Atque soluta hic consequuntur eum repellendus ad.</p>
+                     <span className="text-2xl text-designColor">
+                     <FaLongArrowAltRight/>
+                     </span>
+                     
+                  </div>
+                 
+                    </div>
+                  
+                  </div>
+                 
+                </div>
+                <div className='flex flex-col gap-4 shadow-shadowOne h-80 w-full px-10 py-8 '>
+                  <div className='h-72 overflow-y-hidden'>
+                    <div className='feature-box h-full translate-y-14 bg-slate-100 flex flex-col gap-4'>
+                      <div  className=' w-10 h-8 flex flex-col justify-between '>
+                      <SiProgress className='text-4xl text-designColor'/>
+                      </div>
+                  <div className='flex flex-col gap-6' >
+                  <h3 className="text-xl md:text-2xl font-titleFont font-bold text-gray-300">Business Stratagy</h3>
+                  <p className='font-titleFont text-lightText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Atque soluta hic consequuntur eum repellendus ad.</p>
+                     <span className="text-2xl text-designColor">
+                     <FaLongArrowAltRight/>
+                     </span>
+                     
+                  </div>
+                 
+                    </div>
+                  
+                  </div>
+                 
+                </div>
+                <div className='flex flex-col gap-4 shadow-shadowOne h-80 w-full px-10 py-8 '>
+                  <div className='h-72 overflow-y-hidden'>
+                    <div className='feature-box h-full translate-y-14 bg-slate-100 flex flex-col gap-4'>
+                      <div  className=' w-10 h-8 flex flex-col justify-between '>
+                      <FaMobile className='text-4xl text-designColor'/>
+                      </div>
+                  <div className='flex flex-col gap-6' >
+                  <h3 className="text-xl md:text-2xl font-titleFont font-bold text-gray-300">Business Stratagy</h3>
+                  <p className='font-titleFont text-lightText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Atque soluta hic consequuntur eum repellendus ad.</p>
+                     <span className="text-2xl text-designColor">
+                     <FaLongArrowAltRight/>
+                     </span>
+                     
+                  </div>
+                 
+                    </div>
+                  
+                  </div>
+                 
+                </div>
+                <div className='flex flex-col gap-4 shadow-shadowOne h-80 w-full px-10 py-8 '>
+                  <div className='h-72 overflow-y-hidden'>
+                    <div className='feature-box h-full translate-y-14 bg-slate-100 flex flex-col gap-4'>
+                      <div  className=' w-10 h-8 flex flex-col justify-between '>
+                      <SiAntdesign className='text-4xl text-designColor'/>
+                      </div>
+                  <div className='flex flex-col gap-6' >
+                  <h3 className="text-xl md:text-2xl font-titleFont font-bold text-gray-300">Business Stratagy</h3>
+                  <p className='font-titleFont text-lightText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Atque soluta hic consequuntur eum repellendus ad.</p>
+                     <span className="text-2xl text-designColor">
+                     <FaLongArrowAltRight/>
+                     </span>
+                     
+                  </div>
+                 
+                    </div>
+                  
+                  </div>
+                 
+                </div>
+                <div className='flex flex-col gap-4 shadow-shadowOne h-80 w-full px-10 py-8 '>
+                  <div className='h-72 overflow-y-hidden'>
+                    <div className='feature-box h-full translate-y-14 bg-slate-100 flex flex-col gap-4'>
+                      <div  className=' w-10 h-8 flex flex-col justify-between '>
+                      <FaGlobe className='text-4xl text-designColor'/>
+                      </div>
+                  <div className='flex flex-col gap-6' >
+                  <h3 className="text-xl md:text-2xl font-titleFont font-bold text-gray-300">Business Stratagy</h3>
+                  <p className='font-titleFont text-lightText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Atque soluta hic consequuntur eum repellendus ad.</p>
+                     <span className="text-2xl text-designColor">
+                     <FaLongArrowAltRight/>
+                     </span>
+                     
+                  </div>
+                 
+                    </div>
+                  
+                  </div>
+                 
+                </div>
+                
+                 </div>
+            </div>
+         
+    </section>
   )
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,93 +1,93 @@
-import React, { useState } from 'react';
-import { Link } from 'react-scroll';
-import { logo } from '../Assets/images';
+import React from 'react'
+import { Link } from 'react-scroll'
+import { logo } from '../Assets/images'
 import { MdMenu } from "react-icons/md";
-import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { useState } from 'react';
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 
-export default function Navbar() {
-    const [open, setOpen] = useState(false);
-    const showMenu = () => {
-        setOpen(!open);
-    }
 
-    return (
-        <nav className='w-full sticky bg-bodyColor z-20 top-0 h-24 text-lightText flex justify-between items-center border-b-[1px] border-b-gray-600'>
-            <Link to='home' smooth={true} duration={500}>
-                <img src={logo} alt='logo' />
-            </Link>
-            <ul className='hidden items-center gap-6 mdl:flex cursor-pointer'>
-                <li>
-                    <Link to='home' smooth={true} duration={500}>Home</Link>
-                </li>
-                <li>
-                    <Link to='features' smooth={true} duration={500}>Features</Link>
-                </li>
-                <li>
-                    <Link to='projects' smooth={true} duration={500}>Projects</Link>
-                </li>
-                <li>
-                    <Link to='resume' smooth={true} duration={500}>Resume</Link>
-                </li>
-                <li>
-                    <Link to='testimonials' smooth={true} duration={500}>Testimonials</Link>
-                </li>
-                <li>
-                    <Link to='contact' smooth={true} duration={500}>Contact</Link>
-                </li>
+
+export default function Navbar() {
+    const [open,Setopen]=useState(false)
+    const showmenu=()=>{
+        Setopen(!open)
+    }
+  return (
+    <nav className='w-full sticky  bg-bodyColor z-20 top-0 h-24 text-lightText flex justify-between items-center border-b-[1px] border-b-gray-600 '>
+        <Link to='home' smooth={true} duration={500}>
+        <img src={logo} alt='logo' />
+        </Link>
+      <ul className='hidden items-center gap-6 mdl:flex  cursor-pointer'>
+        <li className='nav-list'>
+            <Link to='home' smooth={true} duration={500} >Home</Link>
+        </li>
+        <li >
+            <Link to='features'smooth={true} duration={500}>Features</Link>
+        </li>
+        <li >
+            <Link to='projects'smooth={true} duration={500}>Projects</Link>
+        </li>
+        <li >
+            <Link to='resume'smooth={true} duration={500}>Resume</Link>
+        </li>
+        <li >
+            <Link to='testmonial'smooth={true} duration={500}>Testmonial</Link>
+        </li>
+        <li >
+            <Link to='contact'smooth={true} duration={500}>Contact</Link>
+        </li>
+      </ul>
+      <span onClick={showmenu} className=' cursor-pointer w-10 h-10 mdl:hidden text-xl bg-black rounded-3xl flex justify-center items-center text-designColor'>
+      <MdMenu />
+      </span>
+      {open&&(
+        <div className='w-[80%] h-screen absolute top-0 left-0 bg-gray-900 p-4 '>
+          <div className='flex flex-col gap-5 relative'>
+          <img className="w-32" src={logo} alt="logo" />
+          <p className='mb-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Earum soluta perspiciatis molestias enim cum repellat, magnam
+                  exercitationem distinctio aliquid nam.</p>
+          </div>
+          <div>
+            <ul className='flex flex-col gap-5 cursor-pointer'>
+              <li>
+                <Link to='home'smooth={true} duration={500} onClick={showmenu}>Home</Link>
+                
+              </li>
+              <li >
+            <Link to='Features'smooth={true} duration={500} onClick={showmenu}>Features</Link>
+        </li>
+        <li >
+            <Link to='Projects'smooth={true} duration={500} onClick={showmenu}>Projects</Link>
+        </li>
+        <li >
+            <Link to='Resume'smooth={true} duration={500} onClick={showmenu}>Resume</Link>
+        </li>
+        <li >
+            <Link to='Testmonial'smooth={true} duration={500} onClick={showmenu}>Testmonial</Link>
+        </li>
+        <li >
+            <Link to='Contact'smooth={true} duration={500} onClick={showmenu}>Contact</Link>
+        </li>
             </ul>
-            <span onClick={showMenu} className='cursor-pointer w-10 h-10 mdl:hidden text-xl bg-black rounded-3xl flex justify-center items-center text-designColor'>
-                <MdMenu />
-            </span>
-            {open && (
-                <div className='w-[80%] h-screen absolute top-0 left-0 bg-gray-900 p-4'>
-                    <div className='flex flex-col gap-5 relative'>
-                        <img className="w-32" src={logo} alt="logo" />
-                        <p className='mb-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                            Earum soluta perspiciatis molestias enim cum repellat, magnam
-                            exercitationem distinctio aliquid nam.</p>
-                    </div>
-                    <div>
-                        <ul className='flex flex-col gap-5 cursor-pointer'>
-                            <li>
-                                <Link to='home' smooth={true} duration={500} onClick={showMenu}>Home</Link>
-                            </li>
-                            <li>
-                                <Link to='features' smooth={true} duration={500} onClick={showMenu}>Features</Link>
-                            </li>
-                            <li>
-                                <Link to='projects' smooth={true} duration={500} onClick={showMenu}>Projects</Link>
-                            </li>
-                            <li>
-                                <Link to='resume' smooth={true} duration={500} onClick={showMenu}>Resume</Link>
-                            </li>
-                            <li>
-                                <Link to='testimonials' smooth={true} duration={500} onClick={showMenu}>Testimonials</Link>
-                            </li>
-                            <li>
-                                <Link to='contact' smooth={true} duration={500} onClick={showMenu}>Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 className='my-5'>FIND ME IN</h3>
-                        <div className='flex gap-4'>
-                            <span className='home-icon'>
-                                <a href="https://www.facebook.com/legendof.ahmed" ><FaFacebook className='icon-1' /></a>
-                            </span>
-                            <span className='home-icon'>
-                                <a href="https://www.linkedin.com/in/ahmed-arafa-155768188/?trk=public_profile-settings_edit-profile-content&original_referer=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2F&originalSubdomain=eg" target="_blank" rel="noopener noreferrer"><FaLinkedin className='icon-1' /></a>
-                            </span>
-                            <span className='home-icon'>
-                                <a href="https://twitter.com" ><FaTwitter className='icon-1' /></a>
-                            </span>
-                        </div>
-                        <span onClick={showMenu}>
-                            <IoMdClose className='text-3xl absolute top-8 right-4 cursor-pointer' />
-                        </span>
-                    </div>
-                </div>
-            )}
-        </nav>
-    );
+          </div>
+          <div>
+            <h3 className='my-5'>FIND ME IN</h3>
+            
+            <div className='flex  gap-4'>
+        <span className='home-icon'> <a href="https://www.facebook.com/legendof.ahmed"><FaFacebook className='icon-1'/></a></span>
+        <span className='home-icon'><a href="https://www.linkedin.com/in/ahmed-arafa-155768188/?trk=public_profile-settings_edit-profile-content&original_referer=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2F&originalSubdomain=eg"><FaLinkedin className='icon-1'/></a></span>
+        <span className='home-icon'> <FaTwitter className='icon-1'/></span>
+            </div>
+            <sapn onClick={showmenu}>
+              <IoMdClose className='text-3xl absolute top-8 right-4 cursor-pointer'/>
+            </sapn>
+          </div>
+        </div>
+      )}
+    </nav>
+  )
 }
