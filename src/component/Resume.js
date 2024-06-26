2db84c7de172fdfa3e8db6696c09d94114d170e4
ahmed arafa <70607269+Ahmed14112017@users.noreply.github.com>
2024-06-26 03:44:26 +0300

    4

--- src/component/Resume.js
+++ src/component/Resume.js
@@ -47,16 +47,16 @@ export default function Resume() {
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <div className='flex'>
-          <div className='w-10 h-[6px] bg-black opacity-30 mt-16  relative'>
-          <span className="absolute w-5 h-5 rounded-full -top-2 -left-3 flex justify-center items-center bg-black bg-opacity-60">
-          <span className="w-3 h-3 rounded-full bg-bodyColor inline-flex group-hover:bg-designColor duration-300"></span>
+          <div className='w-10 h-[6px] bg-black opacity-30 mt-16  relative '>
+          <span className="absolute w-5 h-5 rounded-full -top-2 -left-3 flex justify-center items-center bg-black  bg-opacity-60">
+          <span className="resume-rounded w-3 h-3 rounded-full bg-bodyColor inline-flex group-hover:bg-designColor duration-300"></span>
           </span>
           
           </div>
-          <div className='p-5 bg-black bg-opacity-20 hover:bg-opacity:40 duration-400 rounded-lg w-[500px] shadow-shadowOne text-lightText flex flex-col gap-5'>
-            <div className='w-full flex flex-row justify-between items-center'>
+          <div className='resume-content p-5 bg-black bg-opacity-20 hover:bg-opacity-30 duration-500 rounded-lg w-[500px] shadow-shadowOne text-lightText flex flex-col gap-5'>
+            <div className='flex flex-col lgl:flex-row justify-between gap-4 lgl:gap-0 lgl:items-center'>
               <div className='p-3'>
-              <h3 className='text-xl'>BSc in Computer Science</h3>
+              <h3 className='text-xl md:text-2xl group-hover:text-white duration-300'>BSc in Computer Science</h3>
               <span>University of DVI (2006 - 2010)</span>
               </div>
               <div className='p-3'>
